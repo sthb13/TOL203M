9762e79602db2e173c291c960dd13d764ed3d7ef
Skúli Þór Bjarnason <sthb13@hi.is>
2022-02-06 23:04:33 +0000

    fix title

--- Verkefni1/level.js
+++ Verkefni1/level.js
@@ -1,8 +1,8 @@
 class Level {
     constructor(x, y) {
         this.pos = {x: x, y: y};
-        this.vertices = new Float32Array([-1.0, -0.10, -1.0, 0.05, 1.0, 0.05,
-                                          1.0, 0.05, -1.0, -0.10, 1.0, -0.10]);
+        this.vertices = new Float32Array([-1.0, 0.05, -1.0, -0.10,
+                                          1.0, 0.05, 1.0, -0.10]);
         this.color = vec4(1.0, 0.8, 0.7, 1.0);
         gl.bindBuffer(gl.ARRAY_BUFFER, levelBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, flatten(this.vertices), gl.DYNAMIC_DRAW);
@@ -16,6 +16,6 @@ class Level {
         gl.vertexAttribPointer(pLocation, 2, gl.FLOAT, false, 0, 0);
         gl.uniform2fv(position, flatten([this.pos.x, this.pos.y]));
         gl.uniform4fv(cLocation, flatten(this.color));
-        gl.drawArrays(gl.TRIANGLES, 0, 6);
+        gl.drawArrays(gl.TRIANGLE_STRIP, 0, 6);
     }
 }
